91a15e9431477dea8852c6f23e61e9df6763740d
wangjiabao <840028401@qq.com>
2024-07-11 01:30:45 +0800

    ok

--- app/app/internal/biz/user.go
+++ app/app/internal/biz/user.go
@@ -492,7 +492,7 @@ func (uuc *UserUseCase) AdminRewardList(ctx context.Context, req *v1.AdminReward
 
 		res.Rewards = append(res.Rewards, &v1.AdminRewardListReply_List{
 			CreatedAt: vUserReward.CreatedAt.Add(8 * time.Hour).Format("2006-01-02 15:04:05"),
-			Amount:    fmt.Sprintf("%.2f", float64(vUserReward.Amount)/float64(100000)),
+			Amount:    vUserReward.Amount,
 			AmountB:   vUserReward.AmountB,
 			Address:   tmpUser,
 			Reason:    vUserReward.Reason,
